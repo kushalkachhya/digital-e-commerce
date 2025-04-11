41a4e328027981a8f4bbd0413d3b90bc8a06f4c5
kushalkachhya <kushalkachhiya@gmail.com>
2025-04-11 10:40:58 +0530

    Updated React version and fixed warnings

--- emails/[slug]/page.tsx
+++ emails/[slug]/page.tsx
@@ -7,13 +7,19 @@ interface Props {
   };
 }
 
-export default function EmailPreview({ params }: Props) {
-  let emailMarkup: string = "";
+const EmailPreview = async ({ params }: Props) => {
+  let emailMarkup = "";
 
-
-  if (params.slug === 'welcome-email') {
+  if (params.slug === "welcome-email") {
     emailMarkup = await render(<WelcomeEmail />);
   }
 
-  return <div dangerouslySetInnerHTML={{ __html: emailMarkup }} />;
-}
+  return (
+    <div>
+      <h1>Email Preview</h1>
+      <div dangerouslySetInnerHTML={{ __html: emailMarkup }} />
+    </div>
+  );
+};
+
+export default EmailPreview
